fe0b8211638909521aeb73b8611ed6a55d9d398f
pw-02 <watters.patrick@outlook.com>
2025-03-05 12:48:00 -0800

    use_prefetching: False

--- awslambda/vision/create_vision_batch/app.py
+++ awslambda/vision/create_vision_batch/app.py
@@ -115,9 +115,9 @@ def create_minibatch(bucket_name: str, samples: list, transform, s3_client) -> s
         torch.save(minibatch, buffer)
         bytes_minibatch = buffer.getvalue()
         # Encode the serialized tensor with base64
-        compressed_minibatch = lz4.frame.compress(bytes_minibatch)
+        # bytes_minibatch = lz4.frame.compress(bytes_minibatch)
         # compressed_minibatch = compressor.compress(bytes_minibatch)
-    return compressed_minibatch
+    return bytes_minibatch
 
 
 def cache_minibatch_with_retries(redis_client, batch_id, minibatch, max_retries=4, retry_interval=0.1):
